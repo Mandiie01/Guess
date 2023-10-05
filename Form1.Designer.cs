d4ea13a2febd2c54fac185632acf5d3af6cf1033
Mandiie01 <130066724+Mandiie01@users.noreply.github.com>
2023-10-05 10:09:42 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Guess
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblHint = new System.Windows.Forms.Label();
+            this.btnHint = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbRN = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblHints = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(113, 208);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(75, 23);
+            this.btnOK.TabIndex = 1;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(527, 307);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 2;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(680, 307);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblHint
+            // 
+            this.lblHint.AutoSize = true;
+            this.lblHint.Location = new System.Drawing.Point(630, 115);
+            this.lblHint.Name = "lblHint";
+            this.lblHint.Size = new System.Drawing.Size(0, 13);
+            this.lblHint.TabIndex = 4;
+            // 
+            // btnHint
+            // 
+            this.btnHint.Location = new System.Drawing.Point(539, 208);
+            this.btnHint.Name = "btnHint";
+            this.btnHint.Size = new System.Drawing.Size(75, 23);
+            this.btnHint.TabIndex = 5;
+            this.btnHint.Text = "Hint";
+            this.btnHint.UseVisualStyleBackColor = true;
+            this.btnHint.Click += new System.EventHandler(this.btnHint_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(548, 134);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 6;
+            // 
+            // lbRN
+            // 
+            this.lbRN.AutoSize = true;
+            this.lbRN.Location = new System.Drawing.Point(52, 127);
+            this.lbRN.Name = "lbRN";
+            this.lbRN.Size = new System.Drawing.Size(88, 13);
+            this.lbRN.TabIndex = 7;
+            this.lbRN.Text = "Random number:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(169, 127);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(210, 47);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(250, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "DISNEY MOVIES";
+            // 
+            // lblHints
+            // 
+            this.lblHints.AutoSize = true;
+            this.lblHints.Location = new System.Drawing.Point(489, 80);
+            this.lblHints.Name = "lblHints";
+            this.lblHints.Size = new System.Drawing.Size(35, 13);
+            this.lblHints.TabIndex = 10;
+            this.lblHints.Text = "label3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblHints);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbRN);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnHint);
+            this.Controls.Add(this.lblHint);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnOK);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblHint;
+        private System.Windows.Forms.Button btnHint;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbRN;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblHints;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Guess
+{
+    public partial class Form1 : Form
+    {
+        string[] movies = { "Moana", "Descendant", "Cruella" };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random rand;
+        string[] words = { "" };
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            string[] movies = { "Moana", "Descendant", "Cruella" };
+            Random rand = new Random();
+            int index = rand.Next(0, movies.Length);
+            string guess = textBox1.Text;
+            if (guess == movies[index])
+            {
+                MessageBox.Show(movies + "Correct answer.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                
+            }
+            else
+            {
+                MessageBox.Show(movies + "Incorrect answer.", "Failed", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private void btnHint_Click(object sender, EventArgs e)
+        {
+            string[] hints = { "A strong-willed daughter of a chief of a coastal village, who is chosen by the ocean itself to reunite a mystical relic with the goddess Te Fiti.", " It’s a musical fantasy adventure-comedy that features the teenage children of famous Disney villains, such as Maleficent, the Evil Queen, Jafar, and Cruella de Vil. The film explores themes of self-empowerment, friendship, and questioning one’s identity.", " It’s a dark comedy about ambition, greed, corporate sabotage, revenge, class conflict, high fashion, and murder. The movie stars Emma Stone and Emma Thompson in lead roles and is set in 1970s England." };
+            Random random = new Random();
+            int randomIndex = random.Next(0, movies.Length);
+            /*randomIndex = int.Parse(lblHints.Text);*/
+            string randomHint = hints[randomIndex];
+
+
+            for (int i = 1; i == hints.Length; i += 3)
+            {
+                lblHints.Text = {"A strong-willed daughter of a chief of a coastal village,\n who is chosen by the ocean itself to reunite a mystical \n with the goddess Te Fiti.", " It’s a musical fantasy adventure-comedy that features\n the teenage children of famous Disney villains, such as Maleficent,\n the Evil Queen, Jafar, and Cruella de Vil. The film explores\n themes of self-empowerment, friendship, and questioning one’s identity.", " It’s a dark comedy about ambition, greed, corporate \nsabotage, revenge, class conflict, high fashion,\n and murder. The movie stars Emma Stone and Emma \nThompson in lead roles and is set in 1970s England."};
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            lblHints.Text = " ";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Are you sure you want to close?", "Close", MessageBoxButtons.YesNo,MessageBoxIcon.Question) == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
